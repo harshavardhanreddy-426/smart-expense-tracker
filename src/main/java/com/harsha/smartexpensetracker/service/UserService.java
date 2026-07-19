7e79178b4ba29a7bda197a8edd97b171ab55a269
Harsha Vardhan Reddy Nagireddy <nharshavardhanreddy2006@gmail.com>
2026-07-19 10:21:26 +0530

    Implement Spring Security configuration and registration service

---
+++ src/main/java/com/harsha/smartexpensetracker/service/UserService.java
@@ -0,0 +1,31 @@
+package com.harsha.smartexpensetracker.service;
+
+import com.harsha.smartexpensetracker.dto.RegisterRequest;
+import com.harsha.smartexpensetracker.entity.User;
+import com.harsha.smartexpensetracker.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void register(RegisterRequest request) {
+
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        User user = new User();
+
+        user.setFullName(request.getFullName());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        userRepository.save(user);
+    }
+}
